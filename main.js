b071d15328dd6488d6890853cb99dd1107920d05
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-06-26 21:12:42 +0200

    fixed

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
 const exempleArray = [1, "name", 2, null, undefined, "true", 5, 0];
 
-function calc(...numbers) {
+function calc(numbers) {
     let sum = 0;
     let count = 0;
-    for (let index = 0; index < numbers.length; index++) {
-        const result = numbers[index];
-        const noZero = result.filter(num => num !== 0);
-        for (const element of noZero) { 
-            
-            if (typeof element === "number") {
-                sum += element;
-                count++;
-                
-            } 
-                  
-        }       
-    
-    } if (count > 0) {
-        const midlNumber = sum / count;
-        console.log(midlNumber);
-        
-    } else {
-        console.log("array dont have numbers");
+
+    for (const element of numbers) {
+        if (typeof element === "number") {
+            sum += element;
+            count++;
+        }
     }
 
-}  
+    if (count > 0) {
+        const midlNumber = sum / count;
+        console.log(midlNumber);
+    } else {
+        console.log("array doesn't have numbers");
+    }
+}
 
 calc(exempleArray);
